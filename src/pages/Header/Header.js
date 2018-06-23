0888b50fdc669465399b950b05d5e4e2ea830342
Stephen Ruan <Stephen@Stephens-MacBook-Pro.local>
2018-06-22 17:03:48 -0700

    okay

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -16,16 +16,16 @@ class Header extends Component {
 									<div className="col-lg-4 col-12">
 										<img className="image" src="https://s3-media2.fl.yelpcdn.com/bphoto/9pDQeS-UXFejnuavuIHeMg/o.jpg"/>
 									</div>
-									<div className="col-lg-4 col-12">
+									<div className="col-lg-4 d-none d-lg-block">
 										<img className="image" src="https://s3-media2.fl.yelpcdn.com/bphoto/9pDQeS-UXFejnuavuIHeMg/o.jpg"/>
 									</div>
-									<div className="col-lg-4 col-12">
+									<div className="col-lg-4 d-none d-lg-block">
 										<img className="image" src="https://s3-media2.fl.yelpcdn.com/bphoto/9pDQeS-UXFejnuavuIHeMg/o.jpg"/>
 									</div>
 								</div>
 							</div>
 						</div>
-						
+
 						<div className="col-lg-4 col-md-4 col-4">
 							<img src={logo} className="logo"/>
 						</div>
